072082a223f66fb1173768884b69999844ae4dc8
Nils Paredes <nils.parsa@gmail.com>
2022-05-28 21:59:11 -0500

    Optimize

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 cd /Users/nilsparedes/Developer/IDBI/laradock
-docker-compose up -d mariadb nginx redis php-fpm php-fpm73 workspace workspace73
+docker-compose up -d mariadb nginx redis php-fpm workspace
