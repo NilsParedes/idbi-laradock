264ba8df11c49059948bb7686343727d20b946cf
Nils Paredes <nils.parsa@gmail.com>
2022-08-21 17:33:47 -0500

    Add: php-fpm 8

---
+++ build.sh
@@ -0,0 +1,2 @@
+cd /Users/nilsparedes/Developer/IDBI/laradock
+docker-compose build mariadb nginx redis php-fpm php-fpm8 workspace workspace8 rabbitmq
--- nginx/startup.sh
+++ nginx/startup.sh
@@ -3,7 +3,7 @@
 if [ ! -f /etc/nginx/ssl/default.crt ]; then
     openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
       -keyout "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt" -subj "/CN=local.idbi.pe" \
-      -addext "subjectAltName=DNS:backend.test,DNS:delivery.test"
+      -addext "subjectAltName=DNS:backend.test,DNS:delivery.test,DNS:auth.dev"
 fi
 
 # Start crond in background
